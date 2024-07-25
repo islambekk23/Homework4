1e39ecb972156ed00134c264a4f7b8388c047ce7
islambekk23 <islambekkunabaev@gmail.com>
2024-07-26 00:55:16 +0300

    Add files via upload

---
+++ Homework 4.py
@@ -0,0 +1,10 @@
+name = input('Ваше имя: ')   # Я ввёл своё имя: Исламбек
+my_string = f'Меня зовут {name}'
+print(my_string)
+print(my_string . upper())
+print(my_string . lower())
+print(my_string .replace(' ', '') )
+print(my_string .replace('М', 'м'))
+print(my_string .replace('к' , 'К'))
+print(my_string [0])
+print(my_string [-1])
